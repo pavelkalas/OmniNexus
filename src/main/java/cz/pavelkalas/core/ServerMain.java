c38dac05e3bf966d7c3ccf44f79b6239d323670a
Pavel Kalaš (Floxen) <pavelkalas2048@seznam.cz>
2026-07-04 23:05:16 +0200

    Přejmenována třída z ExampleMod na ServerMain

--- src/main/java/cz/pavelkalas/core/ServerMain.java
+++ src/main/java/cz/pavelkalas/core/ServerMain.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package cz.pavelkalas.core;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,8 +7,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION, acceptableRemoteVersions = "*")
-public class ExampleMod {
+@Mod(modid = ServerMain.MODID, name = ServerMain.NAME, version = ServerMain.VERSION, acceptableRemoteVersions = "*")
+public class ServerMain {
 	public static final String MODID = "omninexus";
 	public static final String NAME = "OmniNexus";
 	public static final String VERSION = "1.0";

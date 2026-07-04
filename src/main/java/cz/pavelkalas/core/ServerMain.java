79279b6815177534f63a91260541c37f37cdd34d
Pavel Kalaš (Floxen) <pavelkalas2048@seznam.cz>
2026-07-04 23:01:31 +0200

    Vytvořen forge-1.12.2 projekt

---
+++ src/main/java/cz/pavelkalas/core/ServerMain.java
@@ -0,0 +1,29 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION, acceptableRemoteVersions = "*")
+public class ExampleMod {
+	public static final String MODID = "omninexus";
+	public static final String NAME = "OmniNexus";
+	public static final String VERSION = "1.0";
+
+	private static Logger logger;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		logger = event.getModLog();
+	}
+
+	/**
+	 * Vstupní bod módu.
+	 */
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+}
